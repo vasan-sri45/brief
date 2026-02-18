20e64fdead4c08a7baf4b64688b2d39284e9c067
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-02-18 17:16:44 +0530

    api.js

--- frontend/app/api/api.js
+++ frontend/app/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL +"/api",
+  baseURL: "/api",
   withCredentials: true, // cookie auth only
   timeout: 60000,
 });
