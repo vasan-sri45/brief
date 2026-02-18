4cf89639e6f17f577d9ff6728e623d1953188c75
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-02-18 17:26:28 +0530

    api.js

--- frontend/app/api/api.js
+++ frontend/app/api/api.js
@@ -10,13 +10,38 @@ export const api = axios.create({
    RESPONSE INTERCEPTOR
    (global error handling optional)
 --------------------------------*/
+// api.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     // Example: if session expired
+//     if (err?.response?.status === 401) {
+//       console.log("Session expired");
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+
 api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    // Example: if session expired
-    if (err?.response?.status === 401) {
-      console.log("Session expired");
+  res => res,
+  async (error) => {
+    const config = error.config;
+
+    // retry only once
+    if (!config || config.__retry) {
+      return Promise.reject(error);
     }
-    return Promise.reject(err);
+
+    // network error OR 502 OR 503 OR timeout
+    if (!error.response || error.code === "ECONNABORTED" || error.response?.status >= 500) {
+      config.__retry = true;
+
+      console.log("Server waking up... retrying in 3s");
+
+      await new Promise(r => setTimeout(r, 3000));
+      return api(config);
+    }
+
+    return Promise.reject(error);
   }
 );
