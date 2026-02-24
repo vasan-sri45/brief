4632f737603dcfb164624cb8d1f12aa9709eb996
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-02-24 14:57:22 +0530

    page.jsx

--- frontend/app/user/about/page.jsx
+++ frontend/app/user/about/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import AboutUs from "../../components/common/About";
 
+export const metadata = {
+  title: "About Us | Briefcasse",
+  description: "Learn about Briefcasse business registration services in India.",
+  keywords: ["company registration", "startup registration", "legal services India"],
+};
+
 const page = () => {
   return (
     <>
