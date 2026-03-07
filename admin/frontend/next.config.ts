2d3959288e5d1a90cd0584b4a49193697766f2c2
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-03-07 19:54:52 +0530

    next.config.ts

--- admin/frontend/next.config.ts
+++ admin/frontend/next.config.ts
@@ -29,8 +29,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        // destination: "https://brief-ewyr.onrender.com/api/:path*",
-        destination:"http://localhost:4500/api/:path*",
+        destination: "https://brief-ewyr.onrender.com/api/:path*",
+        // destination:"http://localhost:4500/api/:path*",
       },
     ];
   },
