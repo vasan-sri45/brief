a72569117fb59c0e22b6494097de552a0abdb319
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-04-28 17:30:05 +0530

    sitemap.js

--- frontend/app/sitemap.js
+++ frontend/app/sitemap.js
@@ -3,31 +3,31 @@
 export default function sitemap() {
   return [
     {
-      url: "https://www.briefcasse.com",
+      url: "https://briefcasse.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://www.briefcasse.com/serviced",
+      url: "https://briefcasse.com/serviced",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://www.briefcasse.com/blogs",
+      url: "https://briefcasse.com/blogs",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://www.briefcasse.com/startup",
+      url: "https://briefcasse.com/startup",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://www.briefcasse.com/contact",
+      url: "https://briefcasse.com/contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
