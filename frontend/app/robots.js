51172798dc183fd97638601acda969417a66f5a2
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-05-06 18:10:41 +0530

    robots.js

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -1,19 +1,42 @@
+// // app/robots.js
+// export default function robots() {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//         disallow: [
+//           "/api/",      // API routes Google index வேண்டாம்
+//           "/user/",     // User private pages
+//           "/login/",    // Login page
+//           "/store/",    // Internal store
+//         ],
+//       },
+//     ],
+//     sitemap: "https://briefcasse.com/sitemap.xml",
+//     // host: "https://briefcasse.com",
+//   };
+// }
+
+
 // app/robots.js
 export default function robots() {
   return {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: [
+          "/",
+          "/user/contact", // ✅ Contact page allow
+        ],
         disallow: [
-          "/api/",      // API routes Google index வேண்டாம்
-          "/user/",     // User private pages
-          "/login/",    // Login page
-          "/store/",    // Internal store
+          "/api/",
+          "/user/",    // General user pages block
+          "/login/",
+          "/store/",
         ],
       },
     ],
     sitemap: "https://briefcasse.com/sitemap.xml",
-    // host: "https://briefcasse.com",
   };
 }
