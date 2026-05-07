72e3ba5eb8eea3a737057c1d138dd04e7ac81ea1
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-05-07 14:29:45 +0530

    robots.js

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -4,8 +4,12 @@ export default function robots() {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+         allow: [
+          "/",
+          "/user/contact",  
+        ],
         disallow: [
+           "/user/", 
           "/api/",      // API routes Google index வேண்டாம்
           "/login/",    // Login page
           "/store/",    // Internal store
@@ -18,24 +22,3 @@ export default function robots() {
 }
 
 
-// app/robots.js
-// export default function robots() {
-//   return {
-//     rules: [
-//       {
-//         userAgent: "*",
-//         allow: [
-//           "/",
-//           "/user/contact", // ✅ Contact page allow
-//         ],
-//         disallow: [
-//           "/api/",
-//           "/user/",    // General user pages block
-//           "/login/",
-//           "/store/",
-//         ],
-//       },
-//     ],
-//     sitemap: "https://briefcasse.com/sitemap.xml",
-//   };
-// }
