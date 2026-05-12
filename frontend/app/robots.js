4c3c90ff25f66a28cf4308f15bd7710f20ae81fd
vasan-sri45 <srinivasanaadhi123@gmail.com>
2026-05-12 16:43:41 +0530

    robots.js

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -9,7 +9,7 @@ export default function robots() {
           "/user/contact",  
         ],
         disallow: [
-           "/user/", 
+          //  "/user/", 
           "/api/",      
           "/login/",    
           "/store/",   
