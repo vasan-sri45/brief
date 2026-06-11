22ad1808518a77ab5f26028714cf42c74b1b108b
yettostart <yet2startdm@gmail.com>
2026-06-11 19:00:12 +0530

    next.cofig.ts

--- admin/frontend/next.config.ts
+++ admin/frontend/next.config.ts
@@ -25,7 +25,7 @@
 import type { NextConfig } from "next";
 
 const apiBaseUrl =
-  process.env.NEXT_API_BASE_URL || "http://localhost:4500";
+  process.env.NEXT_API_BASE_URL || "https://brief-ewyr.onrender.coms";
 
 const nextConfig: NextConfig = {
   images: {
