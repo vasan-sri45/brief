11e2808c0a4d90d599668a35fdd8904448d56bea
yettostart <yet2startdm@gmail.com>
2026-06-11 19:04:26 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));                 // handle CORS for all routes [web:219]
-// app.options("/api/{*splat}", cors(corsOptions)); 
+
 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
